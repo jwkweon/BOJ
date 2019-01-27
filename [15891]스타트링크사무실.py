b6dd57867132c63301f01914ba172d5db0972647
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-01-28 05:15:06 +0900

    Create [15891]스타트링크사무실.py

---
+++ [15891]스타트링크사무실.py
@@ -0,0 +1,17 @@
+n = int(input())
+while True:
+    if n == 5:
+        print('64')
+        break
+    elif n == 4:
+        print('4')
+        break
+    elif n == 3:
+        print('4')
+        break
+    elif n == 2:
+        print('17')
+        break
+    else:
+        print('65')
+        break
