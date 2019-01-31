a994dd84503dd623ee28f3731b5ce77d4df7dcaf
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-01-31 22:11:52 +0900

    Create [10039]평균점수.py

---
+++ [10039]평균점수.py
@@ -0,0 +1,3 @@
+print(sum([k if k >= 40 else 40 for k in [int(input()) for i in range(5)]]) // 5)
+print(sum([max(40, k) for k in [int(input()) for i in range(5)]]) // 5)
+print(sum([max(40,k)for k in [int(input())for i in range(5)]])//5)
